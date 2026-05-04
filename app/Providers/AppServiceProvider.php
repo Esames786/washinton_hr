407374e731394b8015b6d33917d6d655656921a1
Mohsin Sajjad <mohsin.sajjad@logiserves.com>
2026-05-04 20:14:36 +0500

    changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,19 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        // Fix "Specified key was too long" on older MySQL/MariaDB
+        Schema::defaultStringLength(191);
     }
 }
