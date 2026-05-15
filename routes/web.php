71947fa0e9f56b5b7276fd99470fca8cd0abe451
Mohsin Sajjad <mohsin.sajjad@logiserves.com>
2026-05-15 21:31:02 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -86,12 +86,12 @@ use App\Http\Controllers\Employee\EmployeeTicketMessageController;
 
 
                 Route::post('employees/change-status', [AdminEmployeeController::class, 'changeStatus'])->name('employees.change-status');
-                Route::post('employees/{employee}/contract', [AdminEmployeeController::class, 'saveContract'])->name('admin.employees.save-contract');
+                Route::post('employees/{employee}/contract', [AdminEmployeeController::class, 'saveContract'])->name('employees.save-contract');
                 Route::get('employees/{employee}/documents', [AdminEmployeeController::class, 'getDocuments'])->name('employees.documents');
                 Route::post('employees/documents/{document}/verify', [AdminEmployeeController::class, 'verify'])->name('employees.documents.verify');
                 Route::post('employees/documents/bulk-verify', [AdminEmployeeController::class, 'bulkVerify'])->name('employees.documents.bulk-verify');
                 Route::post('employees/attach_agent', [AdminEmployeeController::class, 'attach_agent'])->name('employees.attach_agent');
-                Route::resource('employees', AdminEmployeeController::class);
+                Route::resource('employees', AdminEmployeeController::class)->except(['show']);
                 Route::get('employees/show/{id}', [AdminEmployeeController::class, 'show'])->name('employees.show');
 
                 //Roles Route
